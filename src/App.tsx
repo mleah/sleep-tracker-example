3780f22a0abc6bc72a07b96961da182a91608f8d
Mariah Hanson <mleah@users.noreply.github.com>
2021-09-02 10:56:37 -0400

    Add an example select element hooked up to state

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,25 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="appContainer">
-  <div>HELLO WORLD!!</div>
-    </div>
-  );
+    const [selectValue, setSelectValue] = React.useState("");
+
+
+    const onSelect = (e: React.ChangeEvent<HTMLSelectElement>): void => {
+        setSelectValue(e.target.value);
+    }
+
+    return (
+        <div className="appContainer">
+            <div>HELLO WORLD!!</div>
+            <label htmlFor="exampleSelect">example select</label>
+            <select name="exampleSelect" id="select-example" onChange={onSelect} value={selectValue}>
+                <option value="" disabled hidden>Select an option...</option>
+                <option value={1}>Value 1</option>
+                <option value={2}>Value 2</option>
+                <option value={3}>Value 3</option>
+            </select>
+        </div>
+    );
 }
 
 export default App;
