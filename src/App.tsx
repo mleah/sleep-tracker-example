eff515264170fe465c77f36dc03f8679cee4a328
Mariah Hanson <mleah@users.noreply.github.com>
2021-09-02 11:04:56 -0400

    Add button with initial disabled/enabled logic

--- src/App.tsx
+++ src/App.tsx
@@ -9,16 +9,29 @@ function App() {
         setSelectValue(e.target.value);
     }
 
+    const isFormDisabled = selectValue === "";
+
     return (
         <div className="appContainer">
             <div>HELLO WORLD!!</div>
-            <label htmlFor="exampleSelect">example select</label>
-            <select name="exampleSelect" id="select-example" onChange={onSelect} value={selectValue}>
-                <option value="" disabled hidden>Select an option...</option>
-                <option value={1}>Value 1</option>
-                <option value={2}>Value 2</option>
-                <option value={3}>Value 3</option>
-            </select>
+            <form>
+                <label htmlFor="exampleSelect">example select</label>
+                <select name="exampleSelect" id="select-example" onChange={onSelect} value={selectValue}>
+                    <option value="" disabled hidden>Select an option...</option>
+                    <option value={1}>Value 1</option>
+                    <option value={2}>Value 2</option>
+                    <option value={3}>Value 3</option>
+                </select>
+                <button
+                    type="button"
+                    id="calculate-score-button"
+                    disabled={isFormDisabled}
+                    onClick={() => {
+                        console.log("Clicked!!")
+                    }}>
+                    Calculate
+                </button>
+            </form>
         </div>
     );
 }
